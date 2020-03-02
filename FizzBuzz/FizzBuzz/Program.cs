b2bab0ee6a90887e8eec5e6cc9fdb2ee1bd6c710
Martyna Krawczyk <martynakraw@gmail.com>
2020-03-02 12:13:13 +1100

    initial commit

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; i <= 100; i++)
+            {
+                //Console.WriteLine(i);
+
+                if (i % 3  == 0)
+                {
+                    Console.WriteLine("Fizz");
+                } else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                } else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
--- FoundationalPayslip/FoundationalPayslip/Program.cs
+++ FoundationalPayslip/FoundationalPayslip/Program.cs
@@ -12,8 +12,6 @@ namespace FoundationalPayslip
             int grossIncome;
             double roundedSuperValue;
 
-            
-
 
             Console.WriteLine("Please input your name:");
             string name = Console.ReadLine();
