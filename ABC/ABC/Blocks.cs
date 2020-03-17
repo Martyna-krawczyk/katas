e204a831bd12caef637733c01cbd40f9c9436047
Elena Jovchevska <elena.jovchevska@myob.com>
2020-03-17 15:06:54 +1100

    separating static blocks into a new file

---
+++ ABC/ABC/Blocks.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ABC
+{
+    public class Blocks
+    {
+        public static List<string> AllTheBlocks = new List<string>
+        {
+            "B O",
+            "X K",
+            "D Q",
+            "C P",
+            "N A",
+            "G T",
+            "R E",
+            "T G",
+            "Q D",
+            "F S",
+            "J W",
+            "H U",
+            "V I",
+            "A N",
+            "O B",
+            "E R",
+            "F S",
+            "L Y",
+            "P C",
+            "Z M"
+        };
+    }
+}
--- ABC/ABC/Program.cs
+++ ABC/ABC/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,29 +13,7 @@ namespace ABC
 
             List<string> matchedBlocks = new List<string>();
 
-            List<string> blocksArray = new List<string>
-            {
-                "B O",
-                "X K",
-                "D Q",
-                "C P",
-                "N A",
-                "G T",
-                "R E",
-                "T G",
-                "Q D",
-                "F S",
-                "J W",
-                "H U",
-                "V I",
-                "A N",
-                "O B",
-                "E R",
-                "F S",
-                "L Y",
-                "P C",
-                "Z M"
-            };
+            List<string> blocksArray = Blocks.AllTheBlocks;
 
             for (int i = 0; i < blocksArray.Count; i++)
             {
@@ -52,5 +30,3 @@ namespace ABC
         }
     }
 }
-
-
