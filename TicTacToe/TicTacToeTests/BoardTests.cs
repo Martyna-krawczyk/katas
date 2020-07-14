adc1d181205e25f76b03dbbbd3e1793e60ffb9ba
Martyna-Krawczyk <martynakraw@gmail.com>
2020-07-14 17:57:13 +1000

    Board Test written and passing

--- TicTacToe/TicTacToeTests/BoardTests.cs
+++ TicTacToe/TicTacToeTests/BoardTests.cs
@@ -1,9 +1,21 @@
+using System.Collections.Generic;
+using TicTacToe;
 using Xunit;
 
 namespace TicTacToeTests
 {
     public class BoardTests
     {
+        [Fact]
+        public void PrintBoardMethodPrintsCorrectBoardAsGenerated()
+        {
+            var output = new TestOutput();
+            var board = new Board(output);
+            
+            board.PrintBoard();
+
+            Assert.Contains(". . .\n. . .\n. . .", output.CalledText);
+        }
         
     }
 }
