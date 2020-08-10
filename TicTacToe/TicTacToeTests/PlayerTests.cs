9f9d9d467c390455497081e7637245e1cb594884
Martyna-Krawczyk <martynakraw@gmail.com>
2020-08-10 16:43:12 +1000

    First test for computer and human player tokens.

---
+++ TicTacToe/TicTacToeTests/PlayerTests.cs
@@ -0,0 +1,25 @@
+using TicTacToe;
+using Xunit;
+
+namespace TicTacToeTests
+{
+    public class PlayerTests
+    {
+        [Fact]
+        public void ComputerPlayerTokenIsX()
+        {
+            var computer = new Player("Computer", "X");
+            
+            Assert.Equal("X", computer.Token);
+        }
+        [Fact]
+        public void HumanPlayerTokenIsO()
+        {
+            var player = new Player("Player", "O");
+            
+            Assert.Equal("O", player.Token);
+        }
+        
+        //Computer player picks random spot from the board which is available
+    }
+}
