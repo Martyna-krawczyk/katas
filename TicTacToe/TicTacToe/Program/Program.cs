95cf6c0d2e5e12006fec400791e7d604114ef49a
Martyna-Krawczyk <martynakraw@gmail.com>
2020-09-10 11:53:20 +1000

    Helper methods moved to their own class.

---
+++ TicTacToe/TicTacToe/Program/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Resources.WelcomeMessage);
+            
+            var output = new ConsoleOutput();
+            const int boardSize = 3;
+            var board = new Board(boardSize);
+            
+            const int numberOfPlayers = 2;
+            var numberOfComputerPlayers = ProgramInitializer.UserSetsPlayerPreference();
+            
+            var players = new List<Player>();
+            var tokens = new List<string> {"O","X"};
+            ProgramInitializer.InitialisePlayerList(numberOfPlayers, numberOfComputerPlayers, players, tokens, board);
+            
+            var coordinateParser = new CoordinateParser();
+            var validator = new Validator();
+            var game = new Game(output, players, board, coordinateParser, validator);
+            
+            game.Run();
+        }
+    }
+}
